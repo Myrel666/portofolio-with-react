7d3a269d61180d3021dc9349a9ecb8a6558d5cd9
Myrel666 <egaalmirajp@gmail.com>
2023-07-31 22:23:43 +0700

    add something

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,6 @@ const Home = () => {
         <div className="col">
           <h1 className="text-center">You're In Ega's Space!</h1>
           <p className="text-center">This is a Bootstrap-styled paragraph.</p>
-          {/* <button className="btn btn-primary">Click Me</button> */}
         </div>
       </div>
     </div>
--- src/components/Portofolio.jsx
+++ src/components/Portofolio.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import List from '../components/SkillsetList/List';
 
 const Portofolio = () => {
+
+  const items = ['Manajemen Rapat App', 'Skrining App', 'Montern App'];
+
+  const chart = ['Manajemen Rapat App', 'Skrining App', 'Montern App'];
+
     return (
       <div className="container">
         <div className="row">
           <div className="col">
             <h1 className="text-center">Portofolio</h1>
             <p className="text-center">This is a Bootstrap-styled paragraph.</p>
-            {/* <button className="btn btn-primary">Click Me</button> */}
+            <List items={items} />
+            <List items={chart} />
           </div>
         </div>
       </div>
